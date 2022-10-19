7e6dd5e1d0938f17dbb60a1ec9daddc56030836c
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2022-10-18 21:30:44 -0700

    :sparkles: Adds health bar

--- src/characters.py
+++ src/characters.py
@@ -6,7 +6,7 @@ import pygame.time
 from sprite import Sprite, load_image
 from weapons import Weapon, Gun, GUN_OFFSET, Bullet
 
-HEALTH_BAR_OFFSET = (0, -16)
+HEALTH_BAR_OFFSET = (0, -24)
 
 
 class Character(Sprite):
@@ -16,6 +16,8 @@ class Character(Sprite):
     health: float
     ability_cooldown: int
     ability_last_used: int
+    health_bar: Sprite
+    health_bar_bg: Sprite
 
     def __init__(
             self,
@@ -35,15 +37,30 @@ class Character(Sprite):
         self.weapon = weapon
         self.ability_cooldown = ability_cooldown
         self.ability_last_used = -ability_cooldown
+        self.health_bar = Sprite(
+            load_image("red.png", int(self.health / self.max_health * 100), 16),
+            self.x + HEALTH_BAR_OFFSET[0] - int(self.surface.get_width() / 2),
+            self.y + HEALTH_BAR_OFFSET[1]
+        )
+        self.health_bar_bg = Sprite(load_image("black.png", 100, 16), 10, 10)
 
     def shoot(self, position: Tuple[int, int]):
         return self.weapon.shoot(position, self.id)
 
     def take_damage(self, damage: float):
         self.health -= damage
+        self.health_bar.surface = load_image("red.png", int(self.health / self.max_health * 100), 16)
 
     def update(self):
         self.weapon.move_to(self.x + GUN_OFFSET[0], self.y + GUN_OFFSET[1])
+        self.health_bar.move_to(
+            self.x + HEALTH_BAR_OFFSET[0] - int(self.surface.get_width() / 2),
+            self.y + HEALTH_BAR_OFFSET[1]
+        )
+        self.health_bar_bg.move_to(
+            self.x + HEALTH_BAR_OFFSET[0] - int(self.surface.get_width() / 2),
+            self.y + HEALTH_BAR_OFFSET[1]
+        )
 
     def ability(self, sprites: List):
         pass
@@ -51,6 +68,8 @@ class Character(Sprite):
     def show(self):
         super(Character, self).show()
         self.weapon.show()
+        self.health_bar_bg.show()
+        self.health_bar.show()
 
 
 class Cyborg(Character):
