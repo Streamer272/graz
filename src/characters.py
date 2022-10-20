3cdf27516e35946b79783a42aec5a2caaf15deab
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2022-10-19 19:39:48 -0500

    :sparkles: Adds ability cooldown text

--- src/characters.py
+++ src/characters.py
@@ -6,7 +6,11 @@ import pygame.time
 from sprite import Sprite, load_image
 from weapons import Weapon, Gun, GUN_OFFSET, Bullet
 
+pygame.font.init()
+
 HEALTH_BAR_OFFSET = (0, -24)
+ABILITY_COOLDOWN_OFFSET = (0, 58)
+FONT = pygame.font.SysFont("freeserif", 16)
 
 
 class Character(Sprite):
@@ -16,6 +20,7 @@ class Character(Sprite):
     health: float
     ability_cooldown: int
     ability_last_used: int
+    ability_cooldown_text: Sprite
     health_bar: Sprite
     health_bar_bg: Sprite
 
@@ -37,12 +42,9 @@ class Character(Sprite):
         self.weapon = weapon
         self.ability_cooldown = ability_cooldown
         self.ability_last_used = -ability_cooldown
-        self.health_bar = Sprite(
-            load_image("red.png", int(self.health / self.max_health * 100), 16),
-            self.x + HEALTH_BAR_OFFSET[0] - int(self.surface.get_width() / 2),
-            self.y + HEALTH_BAR_OFFSET[1]
-        )
-        self.health_bar_bg = Sprite(load_image("black.png", 100, 16), 10, 10)
+        self.ability_cooldown_text = Sprite(FONT.render("Ready", True, (0, 0, 0)), 0, 0)
+        self.health_bar = Sprite(load_image("red.png", int(self.health / self.max_health * 100), 16), 0, 0)
+        self.health_bar_bg = Sprite(load_image("black.png", 100, 16), 0, 0)
 
     def shoot(self, position: Tuple[int, int]):
         return self.weapon.shoot(position, self.id)
@@ -52,13 +54,24 @@ class Character(Sprite):
         self.health_bar.surface = load_image("red.png", int(self.health / self.max_health * 100), 16)
 
     def update(self):
+        position_according_to = lambda to: -int(to.surface.get_width() / 2) + int(self.surface.get_width() / 2)
+        now = pygame.time.get_ticks()
+        new_ability_cooldown = "Ready"
+        if now - self.ability_last_used < self.ability_cooldown:
+            new_ability_cooldown = f"Ready in {int((self.ability_cooldown - now + self.ability_last_used) / 1000) + 1}"
+        self.ability_cooldown_text.surface = FONT.render(new_ability_cooldown, True, (0, 0, 0))
+
         self.weapon.move_to(self.x + GUN_OFFSET[0], self.y + GUN_OFFSET[1])
+        self.ability_cooldown_text.move_to(
+            self.x + ABILITY_COOLDOWN_OFFSET[0] + position_according_to(self.ability_cooldown_text),
+            self.y + ABILITY_COOLDOWN_OFFSET[1]
+        )
         self.health_bar.move_to(
-            self.x + HEALTH_BAR_OFFSET[0] - int(self.surface.get_width() / 2),
+            self.x + HEALTH_BAR_OFFSET[0] + position_according_to(self.health_bar_bg),
             self.y + HEALTH_BAR_OFFSET[1]
         )
         self.health_bar_bg.move_to(
-            self.x + HEALTH_BAR_OFFSET[0] - int(self.surface.get_width() / 2),
+            self.x + HEALTH_BAR_OFFSET[0] + position_according_to(self.health_bar_bg),
             self.y + HEALTH_BAR_OFFSET[1]
         )
 
@@ -70,12 +83,22 @@ class Character(Sprite):
         self.weapon.show()
         self.health_bar_bg.show()
         self.health_bar.show()
+        self.ability_cooldown_text.show()
 
 
 class Cyborg(Character):
     def __init__(self, x: int, y: int):
         weapon = Gun(x + GUN_OFFSET[0], y + GUN_OFFSET[1])
-        super(Cyborg, self).__init__(30, weapon, 5000, "cyborg.png", x, y, 48, 48)
+        super(Cyborg, self).__init__(
+            health=30,
+            weapon=weapon,
+            ability_cooldown=5000,
+            path="cyborg.png",
+            x=x,
+            y=y,
+            width=48,
+            height=48
+        )
 
     def update(self):
         super(Cyborg, self).update()
